97fca94adf497b59f457084283f14ee527ec6a37
caraballova <vanessa.caraballo@bryanuniversity.edu>
2021-05-18 01:50:00 -0400

    Rock_The_Vote_P3

---
+++ week5/my-app/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { UserContext } from '../context/UserProvider.js'
+
+export default function Navbar() {
+    const { logout } = useContext(UserContext)
+    return (
+        <div className="navbar">
+            <Link to="/profile">Profile</Link>
+            <Link to='/public'>Issues</Link>
+            <button onClick={logout}>Logout</button>
+        </div>
+    )
+}
