a06dbfe621a3bff918ec796e0298b07b31de3a4f
caraballova <vanessa.caraballo@bryanuniversity.edu>
2021-05-21 18:38:20 -0400

    Rock_The_Vote_P4

---
+++ week6/my-app/src/App.js
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import Navbar from "./components/Navbar.js"
+import Auth from './components/Auth.js'
+import Profile from './components/Profile.js'
+import Public from './components/Public.js'
+import { UserContext } from './context/UserProvider.js'
+import ProtectedRoute from "./components/ProtectedRoute.js"
+
+export default function App() {
+  const { token } = useContext(UserContext)
+  return (
+    <div classname='app'>
+      { token && <Navbar />}
+      <Switch>
+        <Route
+          exact path='/'
+          render={() => token? <Redirect to='profile' /> : <Auth />}
+        />
+
+        <ProtectedRoute 
+          path='/profile'
+          component={ Profile }
+          redirectTo='/'
+          token= { token }
+        />
+
+        <ProtectedRoute
+          path='/public'
+          component={ Public }
+          redirectTo='/'
+          token= { token }
+        />
+
+      </Switch>
+    </div>
+  )
+} 
